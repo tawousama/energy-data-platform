f2855954ad4669aeafb3f967874862e4bc57b5ed
kt_amara <kt_amara@esi.dz>
2026-02-12 15:52:04 +0100

    changing nginx configuration to match railway deployement

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -20,9 +20,8 @@ import type {
 } from '../types';
 
 // Configuration de l'URL de base de l'API
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
-const API_BASE_URL = '/api';
-console.log(API_BASE_URL);
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+console.log('API URL:', API_BASE_URL);
 const API_V1 = `${API_BASE_URL}/v1`;
 
 // Instance Axios configurée
