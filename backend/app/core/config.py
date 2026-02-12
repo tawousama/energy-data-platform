190bf9444de2a1b3e840e3a10884d64f4257d267
kt_amara <kt_amara@esi.dz>
2026-02-12 14:36:58 +0100

    changing nginx configuration to match railway deployement

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -27,9 +27,9 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     
     # CORS - Origines autorisées
-    BACKEND_CORS_ORIGINS: List[str] = [
-        "http://localhost:3000",
-        "http://localhost:5173",
+    BACKEND_CORS_ORIGINS: List[str] = ["*"
+        # "http://localhost:3000",
+        # "http://localhost:5173",
     ]
     
     # Database PostgreSQL
